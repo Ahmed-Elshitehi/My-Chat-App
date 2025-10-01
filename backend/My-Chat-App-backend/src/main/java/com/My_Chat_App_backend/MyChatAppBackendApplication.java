41578c3d9202fcf658281174c83fe014c0a90e7f
Ahmed Salah <elshitehi1@gmail.com>
2025-10-01 19:27:00 +0300

    Initialize back end

---
+++ backend/My-Chat-App-backend/src/main/java/com/My_Chat_App_backend/MyChatAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.My_Chat_App_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChatAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChatAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/My-Chat-App-backend/src/test/java/com/My_Chat_App_backend/MyChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.My_Chat_App_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
